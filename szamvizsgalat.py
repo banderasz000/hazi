2718360d053696aca62835fb48bf4772ddbef080
Horváth András <horvath.andras>
2022-05-12 11:01:06 +0200

    elso

---
+++ szamvizsgalat.py
@@ -0,0 +1,53 @@
+def Szamkeres():
+    hiba = True
+    while hiba:
+        hiba = False
+        try:
+            szam = int(input("Kérek egy pozitív egész számot: "))
+            if szam >= 0:
+                return szam
+        except:
+            pass
+        hiba = True
+
+def Osszeg(Szamok):
+    return sum(Szamok)
+
+def Atlag(Szamok):
+    return sum(Szamok) / len(Szamok)
+
+szamok = []
+bekertszam = Szamkeres()
+
+while bekertszam != 0:
+    szamok.append(bekertszam)
+    bekertszam = Szamkeres()
+
+print("5. feladat:")
+print("Számok", szamok)
+
+print("6. feladat:")
+print("A számok összege:", Osszeg(szamok))
+print("A számok átlaga:", Atlag(szamok))
+
+parosszamok = []
+paratlanszamok = []
+
+for elem in szamok:
+    if elem % 2 == 0:
+        parosszamok.append(elem)
+    else:
+        paratlanszamok.append(elem)
+parosszamok.sort()
+
+print("7. feladat:")
+print("Legkisebb páros szám:", min(parosszamok))
+print("Legnagyobb páros szám:", max(parosszamok))
+print("Páros számok száma:", len(parosszamok))
+print("Páros számok növekvő sorrendben:", parosszamok)
+
+print("8. feladat:")
+print("Legkisebb páratalan szám:", min(paratlanszamok))
+print("Legnagyobb páratalan szám:", max(paratlanszamok))
+print("Páratalan számok száma:", len(paratlanszamok))
+print("páratalan számok:", paratlanszamok)
